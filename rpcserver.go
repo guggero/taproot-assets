09fa5c221801b3dbe87c980fd384e5486c944d6d
Oliver Gugger <gugger@gmail.com>
2024-02-19 16:11:14 +0100

    multi: use prevID as input commitment map key
    To avoid confusion and to eventually support multiple virtual packages
    everywhere, we want to key the input commitments by prevID instead of
    input index.

--- rpcserver.go
+++ rpcserver.go
@@ -1853,7 +1853,7 @@ func (r *rpcServer) AnchorVirtualPsbts(ctx context.Context,
 				"commitment: %w", err)
 		}
 
-		inputCommitments[idx] = inputCommitment.Commitment
+		inputCommitments[prevID] = inputCommitment.Commitment
 	}
 
 	resp, err := r.cfg.ChainPorter.RequestShipment(
